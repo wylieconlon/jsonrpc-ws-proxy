e170d52250bbe30f1ef4542690599f146c67a633
Wylie Conlon <wylieconlon@gmail.com>
2018-11-15 14:37:12 -0500

    Allow configuration of multiple servers by yaml

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
-Sets up a websocket proxy to a JSON-RPC server
+Sets up a websocket proxy for any number of language servers.
+
+Each server is run as a subprocess which is connected to by sending the client
+to the URL /<language> based on a configuration file defined locally. For example,
+with the following defined as `servers.yml`:
+
+```
+langservers:
+  python:
+    - python
+    - python-langserver.py
+    - --stdio
+  go:
+    - /usr/local/bin/go
+    - langserver.go
+```
+
+The client would connect to `ws://localhost/python` to get a python language server
 
 Usage:
 
 ```
 npm install
 npm run prepare
-node dist/server.js --port 3000 --remotePath localhost --remotePort 2089
+node dist/server.js --port 3000 --languageServers servers.yml
 ```
-
-The only required flag to the server is `--remotePort`
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,77 @@
 #!/usr/bin/env node
 
-import * as net from 'net';
 import * as ws from 'ws';
 import * as rpc from 'vscode-ws-jsonrpc';
 import * as rpcServer from 'vscode-ws-jsonrpc/lib/server';
 import * as parseArgs from 'minimist';
+import * as http from 'http';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 let argv = parseArgs(process.argv.slice(2));
 
-if (argv.help) {
-  console.log(`Usage: server.js --port 3000 --remotePath localhost -- 2089`);
+if (argv.help || !argv.languageServers) {
+  console.log(`Usage: server.js --port 3000 --languageServers config.yml`);
   process.exit(1);
 }
 
-let serverPath : number = parseInt(argv.port) || 3000;
-let remotePath : string = argv.remotePath || '127.0.0.1';
-let remotePort : number = parseInt(argv.remotePort);
+let serverPort : number = parseInt(argv.port) || 3000;
 
-if (!remotePort) {
-  console.log('--remotePort is required');
+let languageServers;
+try {
+  let parsed = yaml.safeLoad(fs.readFileSync(argv.languageServers), 'utf8');
+  if (!parsed.langservers) {
+    console.log('Your langservers file is not a valid format, see README.md');
+    process.exit(1);
+  }
+  languageServers = parsed.langservers;
+} catch (e) {
+  console.error(e);
   process.exit(1);
 }
 
 const wss : ws.Server = new ws.Server({
-  port: serverPath,
+  port: serverPort,
   perMessageDeflate: false
 }, () => {
-  console.log(`Listening to http and ws requests on ${serverPath}`);
+  console.log(`Listening to http and ws requests on ${serverPort}`);
 });
 
-const localSocket = net.connect(remotePort, remotePath, () => {
-  console.log(`Connected to local server ${remotePath}:${remotePort}`);
-});
-const localReader : rpc.SocketMessageReader = new rpc.SocketMessageReader(localSocket);
-const localWriter : rpc.SocketMessageWriter = new rpc.SocketMessageWriter(localSocket);
-const localConnection : rpcServer.IConnection = rpcServer.createConnection(localReader, localWriter, () => {});
+function toSocket(webSocket: ws): rpc.IWebSocket {
+  return {
+      send: content => webSocket.send(content),
+      onMessage: cb => webSocket.onmessage = event => cb(event.data),
+      onError: cb => webSocket.onerror = event => {
+          if ('message' in event) {
+              cb((event as any).message)
+          }
+      },
+      onClose: cb => webSocket.onclose = event => cb(event.code, event.reason),
+      dispose: () => webSocket.close()
+  }
+}
 
-wss.on('connection', (client : ws) => {
-  // The ws interface is compatible with the expected socket interface, except binaryType support
-  const socket : rpc.IWebSocket = rpc.toSocket(client as any as WebSocket);
-  const connection : rpcServer.IConnection = rpcServer.createWebSocketConnection(socket);
+wss.on('connection', (client : ws, request : http.IncomingMessage) => {
+  let langServer : string[];
+
+  Object.keys(languageServers).forEach((key) => {
+    if (request.url === '/' + key) {
+      langServer = languageServers[key];
+    }
+  });
+  if (!langServer || !langServer.length) {
+    console.error('Invalid language server', request.url);
+    client.close();
+    return;
+  }
+
+  let localConnection = rpcServer.createServerProcess('Example', langServer[0], langServer.slice(1));
+  let socket : rpc.IWebSocket = toSocket(client);
+  let connection = rpcServer.createWebSocketConnection(socket);
   rpcServer.forward(connection, localConnection);
   console.log(`Forwarding new client`);
+  socket.onClose((code, reason) => {
+    console.log('Client closed', reason);
+    localConnection.dispose();
+  });
 });
